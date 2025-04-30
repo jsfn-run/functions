20dfe17e65d6475884a0a06ffe29f60adf9eb363
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2025-04-30 18:17:45 +0200

    fix: make VTT endpoint function configurable

--- functions/vtt/index.mjs
+++ functions/vtt/index.mjs
@@ -1,13 +1,49 @@
+const _ = process.env;
 const outputFormats = /^(json|text)$/;
 
 const C1 = Buffer.from(
-  '--------------------------xMUfH1yXtLKFZScZN18w2W\r\nContent-Disposition: form-data; name="file"; filename="audio.mp3"\r\nContent-Type: application/octet-stream\r\n\r\n',
+  '--------------------------xMUfH1yXtLKFZScZN18w2W\r\nContent-Disposition: form-data; name="file"; filename="audio.mp3"\r\nContent-Type: application/octet-stream\r\n\r\n'
 );
 const C2 = Buffer.from(
-  '\r\n--------------------------xMUfH1yXtLKFZScZN18w2W\r\nContent-Disposition: form-data; name="model"\r\n\r\nwhisper-1\r\n--------------------------xMUfH1yXtLKFZScZN18w2W--\r\n',
+  '\r\n--------------------------xMUfH1yXtLKFZScZN18w2W\r\nContent-Disposition: form-data; name="model"\r\n\r\nwhisper-1\r\n--------------------------xMUfH1yXtLKFZScZN18w2W--\r\n'
 );
 const C3 =
   "multipart/form-data; boundary=------------------------xMUfH1yXtLKFZScZN18w2W";
+
+async function transcribe(input, response) {
+  const { output = "text", type = "" } = input.options;
+
+  if (!outputFormats.test(output)) {
+    return response.reject("Invalid output format: " + output);
+  }
+
+  const buffer = await input.asBuffer();
+  const formData = Buffer.concat([C1, buffer, C2]);
+
+  const res = await fetch(_.VTT_API_ENDPOINT, {
+    method: "POST",
+    body: formData,
+    headers: {
+      "content-type": C3,
+      "content-length": formData.length,
+    },
+  });
+
+  if (!res.ok) {
+    const body = await res.text();
+    return response.reject("Failed to convert: " + body);
+  }
+
+  const responseText = await res.text();
+  const text = JSON.parse(responseText).text;
+
+  if (output === "json") {
+    response.sendJson({ text });
+    return;
+  }
+
+  response.sendText(text);
+}
 
 export default {
   description: "Voice-to-text",
@@ -20,40 +56,7 @@ export default {
         output: "text or json",
         format: "input media type, like audio/mp3",
       },
-      handler: async (input, response) => {
-        const { output = "text", type = "" } = input.options;
-
-        if (!outputFormats.test(output)) {
-          return response.reject("Invalid output format: " + output);
-        }
-
-        const buffer = await input.asBuffer();
-        const formData = Buffer.concat([C1, buffer, C2]);
-
-        const res = await fetch(VTT_API_ENDPOINT, {
-          method: "POST",
-          body: formData,
-          headers: {
-            "content-type": C3,
-            "content-length": formData.length,
-          },
-        });
-
-        if (!res.ok) {
-          const body = await res.text();
-          return response.reject("Failed to convert: " + body);
-        }
-
-        const responseText = await res.text();
-        const text = JSON.parse(responseText).text;
-
-        if (output === "json") {
-          response.sendJson({ text });
-          return;
-        }
-
-        response.sendText(text);
-      },
+      handler: transcribe,
     },
   },
 };
